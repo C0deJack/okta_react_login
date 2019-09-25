a2b6a001924b3f83255de92173d7d8d195d7eab7
jack <jack.wood-pearce@lovehoney.co.uk>
2019-09-25 17:16:35 +0100

    Add okta SSO widget to project to secure route to staff page

--- src/App.js
+++ src/App.js
@@ -1,21 +1,36 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { Security, SecureRoute, ImplicitCallback } from '@okta/okta-react';
 import Navbar from './components/layout/Navbar';
 import Home from './components/pages/Home';
 import Staff from './components/pages/Staff';
+import Login from './components/auth/Login';
 
 import './App.css';
+
+function onAuthRequired({history}) {
+	history.push('/login');
+}
 
 function App() {
 	return (
 		<Router>
-			<div className="App">
-				<Navbar />
-				<div className="container">
-					<Route path="/" exact={true} component={Home} />
-					<Route path="/staff" exact={true} component={Staff} />
+			<Security
+				issuer='https://dev-110465.okta.com/oauth2/default'
+				clientId='0oa1eojfwpsVZ8MNV357'
+				redirectUri={window.location.origin + '/implicit/callback'}
+				onAuthRequired={onAuthRequired}
+			>
+				<div className="App"> 
+					<Navbar />
+					<div className="container">
+						<Route path="/" exact={true} component={Home} />
+						<SecureRoute path="/staff" exact={true} component={Staff} />
+						<Route path='/login' render={() => <Login baseUrl='https://dev-110465.okta.com' />} />
+						<Route path='/implicit/callback' component={ImplicitCallback} />
+					</div>
 				</div>
-			</div>
+			</Security>
 		</Router>
 	);
 }
